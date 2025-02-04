064d40dcdbd3e77c52107109a28ee8ef0cc525d3
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-02-04 23:03:46 +0900

    Add files via upload

---
+++ Java-mid/src/collection/set/test/UniqueNamesTest1.java
@@ -0,0 +1,16 @@
+package collection.set.test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNamesTest1 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(inputArr));
+        for (Integer i : set) {
+            System.out.println(i);
+        }
+    }
+}
