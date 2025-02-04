bcf0d5a275bc1087f56813eaa0d27661d3b84015
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-02-04 23:45:35 +0900

    Add files via upload

---
+++ Java-mid/src/nested/local/LocalOuterV4.java
@@ -0,0 +1,38 @@
+package nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV4 {
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar){
+        int localVar = 1;
+
+        class LocalPrinter implements Printer{
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+        LocalPrinter printer = new LocalPrinter();
+
+        return printer;
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV4 localOuterV1 = new LocalOuterV4();
+        Printer printer = localOuterV1.process(2);
+        printer.print();
+
+        System.out.println("필드 확인");
+        Field[] declaredFields = printer.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println("declaredField = " + declaredField);
+        }
+    }
+}
