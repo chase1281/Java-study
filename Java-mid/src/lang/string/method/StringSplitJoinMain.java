ad64a6e84dd4cf3cb38c96daa5c99c5c82d46a50
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-02-04 23:38:55 +0900

    Add files via upload

---
+++ Java-mid/src/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,17 @@
+package lang.string.method;
+
+public class StringSplitJoinMain {
+    public static void main(String[] args) {
+        String str = "Apple,Banana,Orange";
+        String[] splitStr = str.split(",");
+        for (String s : splitStr) {
+            System.out.println(s);
+        }
+        
+        String joinStr = String.join("-", "A", "B", "C");
+        System.out.println("joinStr = " + joinStr);
+        
+        String result = String.join("-", splitStr);
+        System.out.println("result = " + result);
+    }
+}
