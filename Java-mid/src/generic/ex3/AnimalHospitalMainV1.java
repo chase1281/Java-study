e0f0d0dbc9c54c6405d090d54b34296178a91471
MinJe Kim <121341025+chase1281@users.noreply.github.com>
2025-02-04 23:21:23 +0900

    Add files via upload

---
+++ Java-mid/src/generic/ex3/AnimalHospitalMainV1.java
@@ -0,0 +1,27 @@
+package generic.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV1 {
+    public static void main(String[] args) {
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+        // 개 병원에 고양이 전달 가능한 문제
+
+        dogHospital.set(dog);
+        // 고양이 타입 반환 가능한 문제
+        Dog biggerDog = (Dog) dogHospital.bigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+
+    }
+}
